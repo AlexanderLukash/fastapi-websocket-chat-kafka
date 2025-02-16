a36be7b4891c013a3a3fb0c2596fa213c021938c
AlexanderLukash <sashass20062727@gmail.com>
2025-02-16 13:17:36 +0200

    Fix websocket connect to non-existent chat.

--- app/application/api/v1/messages/websockets/messages.py
+++ app/application/api/v1/messages/websockets/messages.py
@@ -8,7 +8,10 @@ from fastapi.websockets import WebSocket
 from punq import Container
 
 from app.infra.websockets.managers import BaseConnectionManager
+from app.logic.exceptions.messages import ChatNotFoundException
 from app.logic.init import init_container
+from app.logic.mediator.base import Mediator
+from app.logic.queries.messages import GetChatDetailQuery
 
 router = APIRouter(
     prefix="/chats",
@@ -23,6 +26,14 @@ async def websocket_endpoint(
     container: Container = Depends(init_container),
 ):
     connection_manager: BaseConnectionManager = container.resolve(BaseConnectionManager)
+    mediator: Mediator = container.resolve(Mediator)
+
+    try:
+        await mediator.handle_query(GetChatDetailQuery(chat_oid=chat_oid))
+    except ChatNotFoundException as error:
+        await websocket.send_json(data={"error": error.message})
+        await websocket.close()
+
     await connection_manager.accept_connection(websocket=websocket, key=chat_oid)
 
     await websocket.send_text("You are now connected!")
